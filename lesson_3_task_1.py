39e6ea5666a6665063dfde43779dde43bf820bc8
Konst88888 <kip0863@gmail.com>
2023-09-05 19:56:46 +0300

    дополнения

---
+++ lesson_3_task_1.py
@@ -0,0 +1,6 @@
+from user import User
+
+my_user = User(input('Введите имя: '), input('Введите фамилию: '))
+my_user.sayFirst()
+my_user.sayLast()
+my_user.sayFull()
---
+++ smartphone.py
@@ -0,0 +1,7 @@
+class Smartphone:
+    brend = 'None'
+    model = 'None'
+    number = 0
+
+    def __init__(self, brend, model, number):
+            
